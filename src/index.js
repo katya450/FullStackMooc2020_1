aa67fe5521190d75e4f7c667b256d12afbca0539
Katja Lusma <katja.lusma@gmail.com>
2020-03-11 12:09:45 +0200

    1.3

--- src/index.js
+++ src/index.js
@@ -2,22 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
-  const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+    const course = 'Half Stack application development'
+    const part1 = {
+      name: 'Fundamentals of React',
+      exercises: 10
+    }
+    const part2 = {
+      name: 'Using props to pass data',
+      exercises: 7
+    }
+    const part3 = {
+      name: 'State of a component',
+      exercises: 14
+    }
 
   const Header = props => <h1>{props.course}</h1>;
 
   const Content = () => {
     return (
       <div>
-        <Part part={part1} exercisesInPart={exercises1} />
-        <Part part={part2} exercisesInPart={exercises2} />
-        <Part part={part3} exercisesInPart={exercises3} />
+        <Part part={part1.name} exercisesInPart={part1.exercises} />
+        <Part part={part2.name} exercisesInPart={part2.exercises} />
+        <Part part={part3.name} exercisesInPart={part3.exercises} />
       </div>
     );
   };
@@ -34,7 +40,7 @@ const App = () => {
     <div>
       <Header course={course} />
       <Content />
-      <Total totalNumber={exercises1 + exercises2 + exercises3} />
+      <Total totalNumber={part1.exercises + part2.exercises + part3.exercises} />
     </div>
   );
 };
