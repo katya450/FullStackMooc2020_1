43832686e9bd7b32eb8dc035f55566fa3e1b50e1
Katja Lusma <katja.lusma@gmail.com>
2020-03-11 12:58:51 +0200

    1.5

--- src/index.js
+++ src/index.js
@@ -2,30 +2,42 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7
-    },
-    {
-      name: "State of a component",
-      exercises: 14
-    }
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7
+      },
+      {
+        name: "State of a component",
+        exercises: 14
+      }
+    ]
+  };
 
-  const Header = props => <h1>{props.course}</h1>;
+  const Header = props => <h1>{props.course.name}</h1>;
 
-  const Content = () => {
+  const Content = props => {
+    console.log(props);
     return (
       <div>
-        <Part name={parts[0].name} exercisesInPart={parts[0].exercises} />
-        <Part name={parts[1].name} exercisesInPart={parts[1].exercises} />
-        <Part name={parts[2].name} exercisesInPart={parts[2].exercises} />
+        <Part
+          name={props.parts[0].name}
+          exercisesInPart={props.parts[0].exercises}
+        />
+        <Part
+          name={props.parts[1].name}
+          exercisesInPart={props.parts[1].exercises}
+        />
+        <Part
+          name={props.parts[2].name}
+          exercisesInPart={props.parts[2].exercises}
+        />
       </div>
     );
   };
@@ -37,11 +49,12 @@ const App = () => {
   );
 
   const Total = props => {
-    console.log(props.parts[0].exercises);
     return (
       <p>
         Number of exercises{" "}
-        {parts[0].exercises + parts[1].exercises + parts[2].exercises}
+        {props.parts[0].exercises +
+          props.parts[1].exercises +
+          props.parts[2].exercises}
       </p>
     );
   };
@@ -49,8 +62,8 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
