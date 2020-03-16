252c7dd8bd936405470cadc27795a224cefb0eaf
Katja Lusma <katja.lusma@gmail.com>
2020-03-16 13:48:16 +0200

    1.6

--- src/index.js
+++ src/index.js
@@ -1,71 +1,46 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Button = ({ handleClick, text }) => (
+    <button onClick={handleClick}>{text}</button>
+)
+
+const Statistics = ({ good, neutral, bad}) => (
+    <div>
+        <h1>Statistics</h1>
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
+    </div>
+)
 
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7
-      },
-      {
-        name: "State of a component",
-        exercises: 14
-      }
-    ]
-  };
+    // tallenna napit omaan tilaansa
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+    
+    const giveFeedbackP = () => {
+        setGood(good + 1)
+    }
 
-  const Header = props => <h1>{props.course.name}</h1>;
-
-  const Content = props => {
-    console.log(props);
-    return (
-      <div>
-        <Part
-          name={props.parts[0].name}
-          exercisesInPart={props.parts[0].exercises}
-        />
-        <Part
-          name={props.parts[1].name}
-          exercisesInPart={props.parts[1].exercises}
-        />
-        <Part
-          name={props.parts[2].name}
-          exercisesInPart={props.parts[2].exercises}
-        />
-      </div>
-    );
-  };
-
-  const Part = props => (
-    <p>
-      {props.name} {props.exercisesInPart}
-    </p>
-  );
-
-  const Total = props => {
-    return (
-      <p>
-        Number of exercises{" "}
-        {props.parts[0].exercises +
-          props.parts[1].exercises +
-          props.parts[2].exercises}
-      </p>
-    );
-  };
-
+    const giveFeedbackN = (newValue) => {
+        setNeutral(newValue)
+    }
+    const giveFeedbackB = (newValue) => {
+        setBad(newValue)
+    }
   return (
     <div>
-      <Header course={course} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <h1>Give feedback</h1>
+      <Button handleClick={() => giveFeedbackP()} text='good'/>
+      <Button handleClick={() => giveFeedbackN(neutral + 1)} text='neutral'/>
+      <Button handleClick={() => giveFeedbackB(bad + 1)} text='bad'/>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
-  );
-};
+  )
+}
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
